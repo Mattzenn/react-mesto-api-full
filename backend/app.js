f0e43c98518742b6e2889e218a438fc0c8c5d80d
Matvey Zenin <Mattzenn@yandex.ru>
2021-10-09 21:52:33 +0300

    исправление ошибок

--- backend/app.js
+++ backend/app.js
@@ -12,28 +12,12 @@ const { loginValidation, userValidation } = require('./middlewares/validate');
 // eslint-disable-next-line import/order
 const helmet = require('helmet');
 
-// const options = {
-//   origin: [
-//     'http://localhost:3000',
-//   ],
-//   methods: [GET,HEAD,PUT,PATCH,POST,DELETE],
-//   preflightContinue: false,
-//   optionsSuccessStatus: 204,
-//   allowedHeaders: ['Content-Type', 'origin', 'Authorization', 'Accept'],
-// }
-
-// const options = {
-//   origin: [
-//     'http://localhost:3000',
-//     'http://localhost:3001',
-//   ],
-//   methods: ['GET','HEAD','PUT','PATCH','POST','DELETE'],
-//   preflightContinue: false,
-//   optionsSuccessStatus: 204,
-//   allowedHeaders: ['Content-Type','origin', 'Authorization', 'Accept'],
-//   Credential: true,
-// }
-
+app.use(cors({
+  origin: ['http://mattzenn.nomoredomains.club', 'https://mattzenn.nomoredomains.club', 'localhost:3000'],
+  methods: ['GET', 'POST','PUT', 'DELETE', 'PATCH'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+  credentials: true,
+}));
 
 const app = express();
 const NotFound = require('./errors/NotFound');
@@ -43,42 +27,7 @@ const { PORT = 3000 } = process.env;
 // eslint-disable-next-line import/order
 const rateLimit = require('express-rate-limit');
 
-// Массив доменов, с которых разрешены кросс-доменные запросы
-// const allowedCors = [
-//   'http://localhost:3000',
-//   'http://localhost:3001/signup'
-// ];
 
-// app.use((req, res, next) => {
-//   const { origin } = req.headers; // Сохраняем источник запроса в переменную origin
-//   const { method } = req; // Сохраняем тип запроса (HTTP-метод) в соответствующую переменную
-//   // Значение для заголовка Access-Control-Allow-Methods по умолчанию (разрешены все типы запросов)
-//   const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
-//   // сохраняем список заголовков исходного запроса
-//   const requestHeaders = req.headers['access-control-request-headers'];
-//   // проверяем, что источник запроса есть среди разрешённых
-//   if (allowedCors.includes(origin)) {
-//     // устанавливаем заголовок, который разрешает браузеру запросы из любого источника
-//     res.header('Access-Control-Allow-Origin', origin);
-//     res.header('Access-Control-Allow-Credentials', 'true');
-//   }
-//   // Если это предварительный запрос, добавляем нужные заголовки
-//   if (method === 'OPTIONS') {
-//     // разрешаем кросс-доменные запросы любых типов (по умолчанию)
-//     res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
-//     // разрешаем кросс-доменные запросы с этими заголовками
-//     res.header('Access-Control-Allow-Headers', requestHeaders);
-//     // вернет ответ 200 с пустым телом после preflight-запроса
-//     return res.status(200).send();
-//   }
-
-//   next();
-// });
-
-
-// Enable if you're behind a reverse proxy (Heroku, Bluemix, AWS ELB, Nginx, etc)
-// see https://expressjs.com/en/guide/behind-proxies.html
-// app.set('trust proxy', 1);
 
 const limiter = rateLimit({
   windowMs: 15 * 60 * 1000, // 15 minutes
@@ -91,13 +40,6 @@ app.use(helmet());
 
 // app.use('*', cors(options));
 
-
-app.use(cors({
-  origin: ['http://mattzenn.nomoredomains.club', 'https://mattzenn.nomoredomains.club', 'localhost:3000'],
-  methods: ['GET', 'POST','PUT', 'DELETE', 'PATCH'],
-  allowedHeaders: ['Content-Type', 'Authorization'],
-  credentials: true,
-}));
 // app.use(cors(options));
 
 app.use(bodyParser.json());
